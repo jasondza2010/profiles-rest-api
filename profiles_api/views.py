8027f7ccac4caf89d94c305be162dee425291f69
Jason <jasondza2010@gmail.com>
2019-06-08 15:12:46 +0530

    Added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIViews features"""
+        a_simple_list = [
+            'Apple',
+            'Mango',
+            'Cherry'
+        ]
+
+        return Response({'message': 'My First get API', 'a_simple_list': a_simple_list})     # The response will convert the return into a JSON object, hence it should either be dictonary or a list
